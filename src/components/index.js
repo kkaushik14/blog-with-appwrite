223e157bb574b0213a144f0d543ead6e5b30b2b4
kkaushik14 <kumarkaushik871@gmail.com>
2025-08-20 18:50:34 +0530

    feat: add Input, Login, PostCard, and Select components; update index.js to export new components

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,8 @@ import Footer from './Footer/Footer'
 import Container from './container/Container'
 import Logo from './Logo'
 import LogoutBtn from './Header/LogoutBtn'
+import Select from './Select'
+import Button from './Button'
+import Input from './Input'
 
-export { Header, Footer,Container, Logo, LogoutBtn }
+export { Header, Footer,Container, Logo, LogoutBtn, Select, Button, Input }
